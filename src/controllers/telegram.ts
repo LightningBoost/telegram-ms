f884896b56cc8688a228fdad87614d0e87755145
otaviobonder <otaviobonder@gmail.com>
2021-02-18 17:35:54 -0300

    added global listener to main menu

--- src/controllers/telegram.ts
+++ src/controllers/telegram.ts
@@ -1,7 +1,9 @@
-import { Scenes, session } from 'telegraf';
+import { Scenes, session, Markup } from 'telegraf';
 import bot from '../services/telegram';
 import grayscaleScene from '../scenes/grayscale';
 import btcScene from '../scenes/btc';
+
+const { leave } = Scenes.Stage;
 
 const stage = new Scenes.Stage<Scenes.SceneContext>(
   [grayscaleScene, btcScene],
@@ -11,8 +13,19 @@ const stage = new Scenes.Stage<Scenes.SceneContext>(
 bot.use(session());
 bot.use(stage.middleware());
 
+bot.hears(/Main menu/i, (ctx) => {
+  leave<Scenes.SceneContext>();
+  ctx.reply(
+    'Choose an option',
+    Markup.keyboard(['Grayscale', 'Bitcoin stats']).resize()
+  );
+});
+
 bot.command('grayscale', (ctx) => ctx.scene.enter('grayscale'));
+bot.hears('Grayscale', (ctx) => ctx.scene.enter('grayscale'));
+
 bot.command('btc', (ctx) => ctx.scene.enter('btc'));
+bot.hears('Bitcoin stats', (ctx) => ctx.scene.enter('btc'));
 
 bot.command('help', (ctx) =>
   ctx.reply('Here is the list of available commands')
@@ -25,5 +38,6 @@ bot.on('message', (ctx) =>
 );
 
 bot.catch((e, ctx) => {
+  console.log('Error: ', e);
   ctx.reply('Something went wrong with your request');
 });
